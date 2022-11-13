b2ab1471f1fe4f9e33b93d1a34392503c4d586ab
Mateusz Zaniewski <M.ZANIEWSKI1995@GMAIL.COM>
2022-11-13 19:29:03 +0100

    add footer component

---
+++ react-photography/Components/Footer/Footer.tsx
@@ -0,0 +1,58 @@
+import classes from "./Footer.module.css";
+import Link from "next/link";
+import { CheckDateButton } from "../UI/CheckDateButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceAngry } from "@fortawesome/free-solid-svg-icons";
+export const Footer: React.FC = (props) => {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.footer__wrapper}>
+        <div className={classes.footer__description}>
+          <h2>jestem artystką, która tworzy piekne i romantyczne portrety!</h2>
+        </div>
+        <div className={classes.footer__pictures}>
+          <p className={classes.footer__header}>Portfolio</p>
+          <Link href="/gallery" className={classes.footer__link}>
+            Galeria
+          </Link>
+        </div>
+        <div className={classes.footer__nav}>
+          <div className={classes.footer__about}>
+            <p className={classes.footer__header}>O Mnie</p>
+            <ul>
+              <li className={classes.footer__link}>
+                <Link href="/opinion">Opinie</Link>
+              </li>
+              <li className={classes.footer__link}>
+                <Link href="/about">O Mnie</Link>
+              </li>
+              <li className={classes.footer__link}>
+                <Link href="/offert">Oferta</Link>
+              </li>
+              <li className={classes.footer__link}>
+                <Link href="/contact">Kontakt</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={classes.footer__contact}>
+            <p className={classes.footer__header}>Kontakt</p>
+            <div className={classes.footer__link}>Mail</div>
+            <div className={classes.footer__link}>Tel</div>
+            <div className={classes.socials__icons}>
+              <FontAwesomeIcon icon={faFaceAngry} />
+            </div>
+            <div className={classes.socials__icons}>
+              <FontAwesomeIcon icon={faFaceAngry} />
+            </div>
+          </div>
+        </div>
+        <div className={classes.footer__check}>
+          <CheckDateButton />
+        </div>
+        <div className={classes.footer__copywright}>
+          © 2022. Kamila Koziara - Fotograf ślubny
+        </div>
+      </div>
+    </footer>
+  );
+};
--- react-photography/pages/index.tsx
+++ react-photography/pages/index.tsx
@@ -1,5 +1,6 @@
 import { HeaderMobile } from "../Components/Header/HeaderMobile";
 import { HeaderDesktop } from "../Components/Header/HeaderDesktop";
+import { Footer } from "../Components/Footer/footer";
 import { useState, useEffect } from "react";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
     <main>
       {!desktopResolution && <HeaderMobile />}
       {desktopResolution && <HeaderDesktop />}
+      <Footer />
     </main>
   );
 }
