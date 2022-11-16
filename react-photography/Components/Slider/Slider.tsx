5ff0ef896f592160e42839deca472b229f4d8728
Mateusz Zaniewski <M.ZANIEWSKI1995@GMAIL.COM>
2022-11-16 21:36:24 +0100

    add slider functionality

--- react-photography/Components/Slider/Slider.tsx
+++ react-photography/Components/Slider/Slider.tsx
@@ -1,21 +1,53 @@
 import classes from "./Slider.module.css";
+import { useState } from "react";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleRight,
+  faArrowAltCircleLeft,
+} from "@fortawesome/free-solid-svg-icons";
 export const Slider: React.FC = (props) => {
-  return (
-    <section className={classes.slider}>
-      <div className={classes.slider__slider}></div>
-      <div className={classes.slider__wrapper}>
-        <p className={classes.slider__text}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque quam,
-          rerum accusantium iure odio numquam iste, maiores sint beatae eveniet
-          alias odit rem ad ipsum, aut eius vel. Voluptates, eos.
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-          exercitationem nemo ab minus in error corporis autem laudantium,
-          numquam perferendis blanditiis, officia hic aliquam similique eum
-          adipisci recusandae tenetur minima!
-        </p>
-      </div>
-    </section>
-  );
+  type CurrentSlider = number;
+  const [current, setCurrent] = useState<CurrentSlider>(0);
+
+  const sliderData = [
+    { img: "/../../public/img/footerBgc.jpg" },
+    { img: "/../../public/img/footerBgc.jpg" },
+    { img: "/../../public/img/footerBgc.jpg" },
+    { img: "/../../public/img/footerBgc.jpg" },
+  ];
+  const length = sliderData.length;
+  const prevSlide = {
+    setCurrent((current === 0) ? length - 1 : current - 1);
+  };
+  const nextSlide = {
+    setCurrent((current === length - 1) ? 0 : current + 1);
+  };
+  if (!Array.isArray(sliderData) || sliderData.length <= 0) {return null}
+    return (
+      <section className={classes.slider}>
+        <FontAwesomeIcon
+          icon={faArrowAltCircleLeft}
+          className={classes.slider__left}
+          onClick={() => prevSlide}
+        />
+        <FontAwesomeIcon
+          icon={faArrowAltCircleRight}
+          className={classes.slider__left}
+          onClick={() => nextSlide}
+        />
+
+        {sliderData.map((slide, index) => {
+          return (
+            <Image
+              key={Math.random()}
+              src={slide.img}
+              alt="Random picture"
+              width={500}
+              height={600}
+            />
+          );
+        })}
+      </section>
+    );
 };
