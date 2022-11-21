d8145d6e65cfca50b853dd0460a3b1b1a13e65ef
Mateusz Zaniewski <M.ZANIEWSKI1995@GMAIL.COM>
2022-11-21 22:16:34 +0100

    add data folder

---
+++ react-photography/Data/Data.tsx
@@ -0,0 +1,19 @@
+import classes from "../Components/Nav/NavMobile.module.css";
+export const navigation = [
+  {
+    text: "Oferta",
+    path: "/offert",
+  },
+  {
+    text: "O Mnie ",
+    path: "/about",
+  },
+  {
+    text: "Opinie",
+    path: "/opinion",
+  },
+  {
+    text: "Kontakt",
+    path: "/contact",
+  },
+];
